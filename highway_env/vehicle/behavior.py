c67999c36d8a3e1257c903a936fde51dde41a777
Samir Wadhwa <samir.wdhw@gmail.com>
2020-11-30 14:49:57 -0600

    Update behavior.py

--- highway_env/vehicle/behavior.py
+++ highway_env/vehicle/behavior.py
@@ -499,17 +499,6 @@ class DefensiveVehicle(LinearVehicle):
                                MERGE_ACC_GAIN / (MERGE_VEL_RATIO * MERGE_TARGET_VEL),
                                2.0]
 
-from typing import Tuple, Union
-
-import numpy as np
-
-from highway_env.road.road import Road, Route, LaneIndex
-from highway_env.types import Vector
-from highway_env.vehicle.controller import ControlledVehicle
-from highway_env import utils
-from highway_env.vehicle.kinematics import Vehicle
-from highway_env.road.objects import RoadObject
-
 
 class AutonomousVehicle(ControlledVehicle):
     """
